e58eb8afcbd2b2b8018e828853dd23104b572031
Maddy-133 <mailtomehdi133@gmail.com>
2026-05-04 16:57:18 +0530

    backend added

--- Terraform code/backend.tf
+++ Terraform code/backend.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
   terraform {
   backend "azurerm" {
     resource_group_name  = "mehdi_rg"
@@ -6,14 +5,5 @@
     container_name       = "mehdicontainer"
     key                  = "terraform.tfstate"
   }
-=======
-#   terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "apna_rg"
-#     storage_account_name = "apnawalastghai1996"
-#     container_name       = "tfstate"
-#     key                  = "terraform.tfstate"
-#   }
->>>>>>> 84c4dd2d0aaf62213eeb7c83848d7a8415bcc0c8
 
 }
